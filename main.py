134a0259f51482c163022ecfd6619e12f4f4f943
Aashish-Dahal <ashishdahal490@gmail.com>
2021-06-04 12:26:01 +0545

    setup and Api into section

---
+++ main.py
@@ -0,0 +1,26 @@
+#installation Step 
+#if you want to install fastapi in your machine follow this link:
+#https://fastapi.tiangolo.com/
+
+# Api basic intro section 
+
+from fastapi import FastAPI
+
+# same as express
+app =FastAPI()
+
+@app.get('/') #path('/') & get method is operatin
+
+#def is the keyword for defining a function
+#index is path operation function
+def index():return {
+    'info':'FastApi get method',
+     'data':{
+         'name':'Ashish'
+     }
+}
+@app.get('/about')
+def about():
+    return {'data':'about page' }
+    
+
